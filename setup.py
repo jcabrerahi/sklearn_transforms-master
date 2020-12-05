e677fe8c23697cd57c0d488b5353ddd801f01041
jFury593 <jcabrerahi@gmail.com>
2020-12-04 23:23:17 -0500

    cambio del setup

--- setup.py
+++ setup.py
@@ -8,12 +8,15 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/jcabrerahi/sklearn_transforms-master/',
+      author='Julio Cabrera',
+      author_email='jcabrerahi@gmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
       ],
+      # install_requires=[
+      #    'my_dependency==1.1.0'
+      # ],
       zip_safe=False
 )
